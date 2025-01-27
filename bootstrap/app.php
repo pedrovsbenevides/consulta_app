509c1aa95784063a791d66275655f87e9e47039e
Pedro Benevides <pvsbenevides197@gmail.com>
2025-01-26 21:59:07 -0300

    feat: adiciona sanctum para endpoints stateless

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        using: function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->namespace('App\Http\Controllers')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->prefix('web')
+                ->group(base_path('routes/web.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
