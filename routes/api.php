96c52c7ff83e7324b8137f94fdc18f1b5ae22f9b
Pedro Benevides <pvsbenevides197@gmail.com>
2025-01-27 00:04:09 -0300

    feat(api): adiciona endpoints: * listagem de medicos * listagem de medicos por cidade * cadastro de medico

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,16 @@ Route::group(['prefix' => 'auth'], function () {
 
 Route::group(['prefix' => 'cidades'], function () {
     Route::get('/{nome?}', 'CidadeController@listar');
+    Route::get('/{cidadeId}/medicos/{nome?}', 'MedicoController@listarPorCidade');
+});
+
+Route::group(['prefix' => 'medicos'], function () {
+    Route::get('/{nome?}', 'MedicoController@listar');
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+
+    Route::group(['prefix' => 'cidades'], function () {
+        Route::post('/{cidadeId}/medicos/cadastrar', 'MedicoController@cadastrar');
+    });
 });
