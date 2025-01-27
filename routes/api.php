c7fbaae2fa1f8b3d123e19e1c1b6fc2cdd4c37c9
Pedro Benevides <pvsbenevides197@gmail.com>
2025-01-26 22:41:49 -0300

    feat(api): implementa endpoint de listagem de cidades com nome opcional

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/login', 'AuthController@login');
 });
+
+Route::group(['prefix' => 'cidades'], function () {
+    Route::get('/{nome?}', 'CidadeController@listar');
+});
